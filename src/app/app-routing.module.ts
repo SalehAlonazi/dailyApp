1782c7e77e45d8d87ebb465f62b7b1207a49b78e
Saleh <salehmoazy@gmail.com>
2022-01-30 21:11:51 +0300

    created and add some pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './components/register-page/register-page.component'
 
 const routes: Routes = [];
 
@@ -7,4 +10,12 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  routes: Routes = [
+    { path: './login-page', component: LoginPageComponent },
+    { path: './register-page', component: RegisterPageComponent },
+  ];
+
+}
+
